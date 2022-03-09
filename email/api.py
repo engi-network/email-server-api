218a1f0540f117e7bc8123d5b47c0425535f8ed6
CHRISTOPHER KELLY <chris@nourishbalancethrive.com>
2022-03-09 08:18:58 -0800

    rename topic to contact_list_name; add topics

--- email/api.py
+++ email/api.py
@@ -10,7 +10,8 @@ ses_client = boto3.client("sesv2")
 
 parser = reqparse.RequestParser()
 parser.add_argument("email", type=str, help="email address")
-parser.add_argument("topic", type=str, help="contact list topic")
+parser.add_argument("contact_list_name", type=str, help="contact list name")
+parser.add_argument("topics", action="append", help="list of topics")
 
 
 class Ping(Resource):
@@ -24,6 +25,7 @@ def marshal(r):
         "ContactListName": fields.Raw,
         "EmailAddress": fields.Raw,
         "TopicDefaultPreferences": fields.Raw,
+        "TopicPreferences": fields.Raw,
         "UnsubscribeAll": fields.Raw,
         "CreatedTimestamp": fields.DateTime(dt_format="iso8601"),
         "LastUpdatedTimestamp": fields.DateTime(dt_format="iso8601"),
@@ -33,37 +35,44 @@ def marshal(r):
 
 class Contact(Resource):
     def get(self):
-        """Get an existing `email` on list `topic`"""
+        """Get an existing `email` on list `contact_list_name`"""
         args = parser.parse_args()
         app.logger.info(f"getting {args=}")
         try:
             return marshal(
-                ses_client.get_contact(ContactListName=args["topic"], EmailAddress=args["email"])
+                ses_client.get_contact(
+                    ContactListName=args["contact_list_name"], EmailAddress=args["email"]
+                )
             )
         except ses_client.exceptions.NotFoundException:
             abort(404, message="not found")
 
     def post(self):
-        """Create a new `email` on list `topic`"""
+        """Create a new `email` on list `contact_list_name`"""
         args = parser.parse_args()
         app.logger.info(f"adding {args=}")
         try:
             return marshal(
                 ses_client.create_contact(
-                    ContactListName=args["topic"], EmailAddress=args["email"]
+                    ContactListName=args["contact_list_name"],
+                    EmailAddress=args["email"],
+                    TopicPreferences=[
+                        {"TopicName": topic, "SubscriptionStatus": "OPT_IN"}
+                        for topic in args.get("topics", [])
+                    ],
                 )
             )
         except ses_client.exceptions.AlreadyExistsException:
             abort(409, message="already exists")
 
     def delete(self):
-        """Delete `email` from list `topic`"""
+        """Delete `email` from list `contact_list_name`"""
         args = parser.parse_args()
         app.logger.info(f"deleting {args=}")
         try:
             return marshal(
                 ses_client.delete_contact(
-                    ContactListName=args["topic"], EmailAddress=args["email"]
+                    ContactListName=args["contact_list_name"], EmailAddress=args["email"]
                 )
             )
         except ses_client.exceptions.NotFoundException:
--- email/test/test_email_server.py
+++ email/test/test_email_server.py
@@ -2,22 +2,26 @@ import requests
 
 URL = "http://127.0.0.1:5000"
 EMAIL = "christopherkelly@engi.network"
-TOPIC = "engi-newsletter"
-DATA = {"email": EMAIL, "topic": TOPIC}
+CONTACT_LIST_NAME = "engi-newsletter"
+TOPICS = [CONTACT_LIST_NAME]
+DATA = {"email": EMAIL, "contact_list_name": CONTACT_LIST_NAME}
 
 
 class TestContact:
     endpoint = f"{URL}/contact"
 
     def test_should_be_able_to_add_contact(self):
-        assert requests.post(TestContact.endpoint, data=DATA).status_code == 200
+        assert (
+            requests.post(TestContact.endpoint, data={**DATA, "topics": TOPICS}).status_code == 200
+        )
 
     def test_should_be_able_to_check_contact(self):
         r = requests.get(TestContact.endpoint, params=DATA)
         assert r.status_code == 200
         meta = r.json()
-        assert meta["ContactListName"] == TOPIC
+        assert meta["ContactListName"] == CONTACT_LIST_NAME
         assert meta["EmailAddress"] == EMAIL
+        assert [d["TopicName"] for d in meta["TopicPreferences"]] == TOPICS
 
     def test_should_be_able_to_delete_contact(self):
         assert requests.delete(TestContact.endpoint, data=DATA).status_code == 200
